327e2437a96042f75144ff17d78b1e9ecf063ec1
Mark O'Hara <Mark@Marks-MacBook-Air.local>
2015-04-08 21:50:44 +0100

    Added Persistent score system  and database

---
+++ src/game/src/Score/Main.java
@@ -0,0 +1,30 @@
+package game.src.Score;
+
+import processing.core.PApplet;
+
+
+public class Main extends PApplet
+{
+	
+	ScoreSearcher searcher;
+	
+	public void setup()
+	{
+		searcher = new ScoreSearcher();
+		searcher.loadScores();
+	}
+	
+	public void draw()
+	{
+
+	}
+	
+	
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+			
+		System.out.println("I am on git now");
+	}
+
+}
