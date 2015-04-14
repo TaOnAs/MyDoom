4dd1d7272920921551e4463fe6b054efc69ee3dc
Ciaran Strutt <0x4000@mail.ru>
2015-04-14 14:27:04 +0100

    Revert "Added in chain gauge, coverarea, increased blocksizes"
    This reverts commit d1e9ab2c4a4b3bf97abfba2ecd440b2ccbe4070e.
    
    Fixing the push trent made which destroyed the entire file structure of the project, good job trent

--- src/game/gamplay/Main.java
+++ src/game/gamplay/Main.java
@@ -1,4 +1,4 @@
-package game.src.gamplay;
+package src.game.gamplay;
 
 import java.util.ArrayList;
 import processing.core.PApplet;
@@ -11,15 +11,10 @@ public class Main extends PApplet
 	int maxNoArrows; //Maximum number of arrows that will be made
 	int spawnSpot;
 	int score;
-	int chainGauge;
-	
-	float arrowSpeedVariable; //Speed of the arrow
-	float speedConstant; //Constant that is needed to adjust arrows to constant speed regardless of screensize or arrow size
-	
 	float life;
 	
-	float ArrowHei = 100; //Sets Arrow height
-	float ArrowLen = 100; //Sets Arrow length
+	float ArrowHei = 50; //Sets Arrow height
+	float ArrowLen = 50; //Sets Arrow length
 	
 	ArrayList<Arrow> Arrows;
 	checkArea scoreSpot;
@@ -27,7 +22,7 @@ public class Main extends PApplet
 	
 	public void setup()
 	{
-		size(800, 1580);
+		size(800, 580);
 		
 		score = 0;
 		life = 100;
@@ -43,8 +38,6 @@ public class Main extends PApplet
 	    noArrows = 0;
 	    maxNoArrows = 32;
 	    
-	    arrowSpeedVariable = 25f;
-	    
 	    //Starts up the buttons as off
 	    buttons[0] = false;
 	    buttons[1] = false;
@@ -58,9 +51,8 @@ public class Main extends PApplet
 	    //Gameplay()
 	    
 	    text(noArrows, 50, 50);
-	    text(life, 100, 210);
-	    text(score, 100, 220);
-	    text("Combo" + chainGauge, 300, 50);
+	    text(life, 100, 110);
+	    text(score, 100, 120);
 	    
 	    scoreSpot.drawArea();
 	    
@@ -70,17 +62,14 @@ public class Main extends PApplet
 	    	{
 	    		spawnSpot = (int)random(0, 4);
 	    		
-		        Arrows.add(new Arrow(ArrowHei, ArrowLen, 200, spawnSpot, noArrows));
+		        Arrows.add(new Arrow(ArrowHei, ArrowHei, 200, spawnSpot, noArrows));
 		        noArrows += 1; //Increments number of arrows
 	    	}
 	    }
 	    
-	    //Sets the arrows speed
     	for(int i = 0; i < Arrows.size(); i++)
     	{
-    		speedConstant = arrowSpeedVariable/Arrows.get(i).arrowHei;
-    		
-    		Arrows.get(i).arrowMovement(arrowSpeedVariable*speedConstant);
+    		Arrows.get(i).arrowMovement(2f);
     	}
 	    
 	    //Checks to see if arrows go off screen
@@ -88,74 +77,6 @@ public class Main extends PApplet
 	    {
 	    	if(Arrows.get(i).arrowOffScreen())
 	    	{
-	    		Arrows.remove(i);
-	    		noArrows -= 1;
-	    	}
-	    }
-	    
-	    for(int i = 0; i < Arrows.size(); i++)
-	    {
-	    	boolean arrowsPressed = false;
-	    	
-	    	//If note is in box and the right key is pressed
-	    	if(collision.collisionConnect(Arrows.get(i).arrowCurrXPos + Arrows.get(i).arrowLen, Arrows.get(i).arrowCurrYPos + Arrows.get(i).arrowHei, Arrows.get(i).arrowHei, Arrows.get(i).arrowLen) && ((Arrows.get(i).arrowCurrYPos > scoreSpot.awayFromTop) && (Arrows.get(i).arrowCurrYPos < scoreSpot.awayFromTop + scoreSpot.checkHei)))
-	    	{
-	    		if((Arrows.get(i).kPos == 0) && (buttons[0]))
-	    		{
-	    			score += scoreSystemInverse(Arrows.get(i).arrowCurrXPos + Arrows.get(i).arrowLen, Arrows.get(i).arrowCurrYPos + Arrows.get(i).arrowHei, Arrows.get(i).arrowHei, Arrows.get(i).arrowLen);
-	    			arrowsPressed = true;
-	    		}
-	    		else if(((Arrows.get(i).kPos == 0)) && ((buttons[1]) || (buttons[2]) || (buttons[3])))
-	    		{
-	    			chainGauge = 0;
-	    			life -= 10;
-	    			arrowsPressed = true;
-	    		}
-	    		
-	    		if((Arrows.get(i).kPos == 1) && (buttons[1]))
-	    		{
-	    			score += scoreSystemInverse(Arrows.get(i).arrowCurrXPos + Arrows.get(i).arrowLen, Arrows.get(i).arrowCurrYPos + Arrows.get(i).arrowHei, Arrows.get(i).arrowHei, Arrows.get(i).arrowLen);
-	    			arrowsPressed = true;
-	    		}
-	    		else if(((Arrows.get(i).kPos == 1)) && ((buttons[0]) || (buttons[3]) || (buttons[2])))
-	    		{
-	    			chainGauge = 0;
-	    			life -= 10;
-	    			arrowsPressed = true;
-	    		}
-	    		
-	    		if((Arrows.get(i).kPos == 2) && (buttons[2]))
-	    		{
-	    			score += scoreSystemInverse(Arrows.get(i).arrowCurrXPos + Arrows.get(i).arrowLen, Arrows.get(i).arrowCurrYPos + Arrows.get(i).arrowHei, Arrows.get(i).arrowHei, Arrows.get(i).arrowLen);
-	    			arrowsPressed = true;
-	    		}
-	    		else if(((Arrows.get(i).kPos == 2)) && ((buttons[0]) || (buttons[3]) || (buttons[1])))
-	    		{
-	    			chainGauge = 0;
-	    			life -= 10;
-	    			arrowsPressed = true;
-	    		}
-	    		
-	    		if((Arrows.get(i).kPos == 3) && (buttons[3]))
-	    		{
-	    			score += scoreSystemInverse(Arrows.get(i).arrowCurrXPos + Arrows.get(i).arrowLen, Arrows.get(i).arrowCurrYPos + Arrows.get(i).arrowHei, Arrows.get(i).arrowHei, Arrows.get(i).arrowLen);
-	    			arrowsPressed = true;
-	    		}
-	    		else if(((Arrows.get(i).kPos == 3)) && ((buttons[1]) || (buttons[2]) || (buttons[0])))
-	    		{
-	    			chainGauge = 0;
-	    			life -= 10;
-	    			arrowsPressed = true;
-	    		}
-	    	}
-	    	
-	    	if(arrowsPressed)
-	    	{
-	    		buttons[0] = false;
-	    	    buttons[1] = false;
-	    	    buttons[2] = false;
-	    	    buttons[3] = false;
-	    		
 	    		Arrows.remove(i);
 	    		noArrows -= 1;
 	    	}
@@ -195,104 +116,49 @@ public class Main extends PApplet
 	    	}*/
 	    	
 	    	//If note is in box and the right key is pressed
-	    	/*if(collision.collisionConnect(Arrows.get(i).arrowCurrXPos + Arrows.get(i).arrowLen, Arrows.get(i).arrowCurrYPos + Arrows.get(i).arrowHei, Arrows.get(i).arrowHei, Arrows.get(i).arrowLen) && ((Arrows.get(i).arrowCurrYPos > scoreSpot.awayFromTop) && (Arrows.get(i).arrowCurrYPos < scoreSpot.awayFromTop + scoreSpot.checkHei)))
+	    	if(collision.collisionConnect(Arrows.get(i).arrowCurrXPos, Arrows.get(i).arrowCurrYPos, Arrows.get(i).arrowHei, Arrows.get(i).arrowLen))
 	    	{
 	    		if((Arrows.get(i).kPos == 0) && (buttons[0]))
 	    		{
-	    			score += scoreSystemInverse(Arrows.get(i).arrowCurrXPos + Arrows.get(i).arrowLen, Arrows.get(i).arrowCurrYPos + Arrows.get(i).arrowHei, Arrows.get(i).arrowHei, Arrows.get(i).arrowLen);
+	    			score += 10;
 	    			arrowsPressed = true;
 	    		}
 	    		else if(((Arrows.get(i).kPos == 0)) && ((buttons[1]) || (buttons[2]) || (buttons[3])))
 	    		{
-	    			chainGauge = 0;
 	    			life -= 10;
 	    			arrowsPressed = true;
 	    		}
 	    		
+	    		
 	    		if((Arrows.get(i).kPos == 1) && (buttons[1]))
 	    		{
-	    			score += scoreSystemInverse(Arrows.get(i).arrowCurrXPos + Arrows.get(i).arrowLen, Arrows.get(i).arrowCurrYPos + Arrows.get(i).arrowHei, Arrows.get(i).arrowHei, Arrows.get(i).arrowLen);
+	    			score += 10;
 	    			arrowsPressed = true;
 	    		}
 	    		else if(((Arrows.get(i).kPos == 1)) && ((buttons[0]) || (buttons[3]) || (buttons[2])))
 	    		{
-	    			chainGauge = 0;
 	    			life -= 10;
 	    			arrowsPressed = true;
 	    		}
 	    		
 	    		if((Arrows.get(i).kPos == 2) && (buttons[2]))
 	    		{
-	    			score += scoreSystemInverse(Arrows.get(i).arrowCurrXPos + Arrows.get(i).arrowLen, Arrows.get(i).arrowCurrYPos + Arrows.get(i).arrowHei, Arrows.get(i).arrowHei, Arrows.get(i).arrowLen);
+	    			score += 10;
 	    			arrowsPressed = true;
 	    		}
 	    		else if(((Arrows.get(i).kPos == 2)) && ((buttons[0]) || (buttons[3]) || (buttons[1])))
 	    		{
-	    			chainGauge = 0;
 	    			life -= 10;
 	    			arrowsPressed = true;
 	    		}
 	    		
 	    		if((Arrows.get(i).kPos == 3) && (buttons[3]))
 	    		{
-	    			score += scoreSystemInverse(Arrows.get(i).arrowCurrXPos + Arrows.get(i).arrowLen, Arrows.get(i).arrowCurrYPos + Arrows.get(i).arrowHei, Arrows.get(i).arrowHei, Arrows.get(i).arrowLen);
+	    			score += 10;
 	    			arrowsPressed = true;
 	    		}
 	    		else if(((Arrows.get(i).kPos == 3)) && ((buttons[1]) || (buttons[2]) || (buttons[0])))
 	    		{
-	    			chainGauge = 0;
-	    			life -= 10;
-	    			arrowsPressed = true;
-	    		}
-	    	}*/
-	    	
-	    	//Checks if bottom is in box
-	    	if((collision.collisionConnect(Arrows.get(i).arrowCurrXPos, Arrows.get(i).arrowCurrYPos, Arrows.get(i).arrowHei, Arrows.get(i).arrowLen)) && ((Arrows.get(i).arrowCurrYPos > scoreSpot.awayFromTop) && (Arrows.get(i).arrowCurrYPos < scoreSpot.awayFromTop + scoreSpot.checkHei)))
-	    	{
-	    		if((Arrows.get(i).kPos == 0) && (buttons[0]))
-	    		{
-	    			score += scoreSystem(Arrows.get(i).arrowCurrXPos, Arrows.get(i).arrowCurrYPos, Arrows.get(i).arrowHei, Arrows.get(i).arrowLen);
-	    			arrowsPressed = true;
-	    		}
-	    		else if(((Arrows.get(i).kPos == 0)) && ((buttons[1]) || (buttons[2]) || (buttons[3])))
-	    		{
-	    			chainGauge = 0;
-	    			life -= 10;
-	    			arrowsPressed = true;
-	    		}
-	    		
-	    		if((Arrows.get(i).kPos == 1) && (buttons[1]))
-	    		{
-	    			score += scoreSystem(Arrows.get(i).arrowCurrXPos, Arrows.get(i).arrowCurrYPos, Arrows.get(i).arrowHei, Arrows.get(i).arrowLen);
-	    			arrowsPressed = true;
-	    		}
-	    		else if(((Arrows.get(i).kPos == 1)) && ((buttons[0]) || (buttons[3]) || (buttons[2])))
-	    		{
-	    			chainGauge = 0;
-	    			life -= 10;
-	    			arrowsPressed = true;
-	    		}
-	    		
-	    		if((Arrows.get(i).kPos == 2) && (buttons[2]))
-	    		{
-	    			score += scoreSystem(Arrows.get(i).arrowCurrXPos, Arrows.get(i).arrowCurrYPos, Arrows.get(i).arrowHei, Arrows.get(i).arrowLen);
-	    			arrowsPressed = true;
-	    		}
-	    		else if(((Arrows.get(i).kPos == 2)) && ((buttons[0]) || (buttons[3]) || (buttons[1])))
-	    		{
-	    			chainGauge = 0;
-	    			life -= 10;
-	    			arrowsPressed = true;
-	    		}
-	    		
-	    		if((Arrows.get(i).kPos == 3) && (buttons[3]))
-	    		{
-	    			score += scoreSystem(Arrows.get(i).arrowCurrXPos, Arrows.get(i).arrowCurrYPos, Arrows.get(i).arrowHei, Arrows.get(i).arrowLen);
-	    			arrowsPressed = true;
-	    		}
-	    		else if(((Arrows.get(i).kPos == 3)) && ((buttons[1]) || (buttons[2]) || (buttons[0])))
-	    		{
-	    			chainGauge = 0;
 	    			life -= 10;
 	    			arrowsPressed = true;
 	    		}
@@ -312,103 +178,6 @@ public class Main extends PApplet
 	    
 	    //Press buttons to match with buttons
 	    
-	}
-	
-	//Calculates score
-	public int scoreSystem(float arrowXPos, float arrowYPos, float arrowHei, float arrowLen)
-	{
-		float closenessToTop;
-		float coverageRatio;
-		float amountCovered;
-		float startingLine;
-		int points = 0;
-		
-		closenessToTop = arrowYPos - scoreSpot.awayFromTop;
-		
-		//0 is completely covered
-		amountCovered = arrowHei - closenessToTop;
-		
-		//Closeness to perfect sync
-		coverageRatio = (amountCovered/arrowHei)*100;
-		
-		//Perfect
-		if(coverageRatio >=90)
-		{
-			chainGauge += 1;
-			points += 1000;
-		}
-		
-		//Great
-		if((coverageRatio < 90) && (coverageRatio >= 50))
-		{
-			chainGauge += 1;
-			points += 800;
-		}
-		
-		//Good
-		if((coverageRatio < 50) && (coverageRatio >= 20))
-		{
-			chainGauge += 1;
-			points += 300;
-		}
-		
-		//Poor
-		if((coverageRatio < 20) && (coverageRatio >= 0))
-		{
-			chainGauge = 0;
-			points += 50;
-			life -= 5;
-		}
-		
-		return points;
-	}
-	
-	public int scoreSystemInverse(float arrowXPos, float arrowYPos, float arrowHei, float arrowLen)
-	{
-		float closenessToTop;
-		float coverageRatio;
-		float amountCovered;
-		float startingLine;
-		int points = 0;
-		
-		closenessToTop = arrowYPos - scoreSpot.awayFromTop;
-		
-		//0 is completely covered
-		amountCovered = arrowHei - closenessToTop;
-		
-		//Closeness to perfect sync
-		coverageRatio = (amountCovered/arrowHei)*100;
-		
-		//Perfect
-		if(coverageRatio >=90)
-		{
-			chainGauge = 0;
-			points += 50;
-			life -= 5;
-		}
-		
-		//Great
-		if((coverageRatio < 90) && (coverageRatio >= 50))
-		{
-			chainGauge += 1;
-			points += 300;
-		}
-		
-		//Good
-		if((coverageRatio < 50) && (coverageRatio >= 30))
-		{
-			chainGauge += 1;
-			points += 800;
-		}
-		
-		//Poor
-		if((coverageRatio < 30) && (coverageRatio >= 0))
-		{
-			chainGauge += 1;
-			points += 1000;
-		}
-		
-		return points;
 	}
 	
 	//Creates the area in which points can be gotten when button is pressed when square is on the area
