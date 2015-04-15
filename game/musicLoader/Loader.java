dd8db3e14d3587e317830d9e069f761a06fca53d
Ciaran Strutt <0x4000@mail.ru>
2015-04-15 12:23:53 +0100

    Made code inheritable into a master PApplet so it is rendered in one Applet and not multiple

--- game/musicLoader/Loader.java
+++ game/musicLoader/Loader.java
@@ -10,35 +10,47 @@
 
 package game.musicLoader;
 
-import java.io.File;
-
 import processing.core.PApplet; //PApplet is in here
 import ddf.minim.*; //Processing sound methods
 import ddf.minim.analysis.FFT;
 
-public class Loader extends PApplet{
+import javax.swing.*;
+
+import java.io.*;
+
+public class Loader{
 
 	public Minim musicLoader; //Create a variable that will be used to instantiate a minim object
 	public AudioPlayer musicPlayer; //Object that will actually play the sound
 	public AudioMetaData musicMetaData;
 	public FFT frequencySpectrum;
+	PApplet parent;
 	public boolean fileLoaded;
 	public int metaXPos,metaYPos,metaSpacing; //Co-ordinates for displaying the metadata on the applet instead of console
+	public String musicFileName;
+	public File musicFile;
+	final JFileChooser fileChooser = new JFileChooser();
+	int fileChooserCheck = fileChooser.showOpenDialog(parent);
 	
 	public void setup()
 	{
-		size(1024,600); //The sceeen size is closely linked the the buffer size grab chunks of music data into minim
-		smooth();
-		noLoop(); //Run draw() once
+		parent.size(1024,600); //The sceeen size is closely linked the the buffer size grab chunks of music data into minim
+		parent.smooth();
+		parent.noLoop(); //Run draw() once
 		/*
 		 * draw() was being ran even before the file was loaded but what was being drawn
 		 * is dependent on data in the music file being loaded 
 		 */
-		fileLoaded = false; 
 		metaXPos = 50; //X cord
-		metaYPos = 150; //Y cord
+		metaYPos = 200; //Y cord
 		metaSpacing = 15; //Spacing between each line of text
-		textFont(createFont("Serif", metaSpacing));
+		parent.textFont(parent.createFont("Serif", metaSpacing));
+		fileLoaded = false;
+	}
+	
+	public Loader(PApplet mainApplet)
+	{
+		parent = mainApplet;
 	}
 	
 	public void showFileDialog()
@@ -49,21 +61,32 @@ public class Loader extends PApplet{
 		 * the second parameter is a callback which means when selectInput is called
 		 * it will then call the method specified in the second parameter
 		 */
-		selectInput("Select a music file to open","getMusicFile");  
+		//parent.selectInput("Select a music file to open","getMusicFile",null,this);  //Callback functions extremely dodgy with multiple classes
+		if(fileChooserCheck == JFileChooser.APPROVE_OPTION)
+		{
+			musicFile = fileChooser.getSelectedFile();
+			musicFileName = musicFile.getAbsolutePath();
+			getMusicFile(musicFile);
+		}
+		else
+		{
+			PApplet.println("Error loading file");
+		}
+		
 	}
 	
 	public void getMusicFile(File musicFile)
 	{
 		if(musicFile.isFile() != true) //Make sure the file points to something, will add audio extension filtering 
 		{
-			println("Error while loading file");
+			PApplet.println("Error while loading file");
 		}
 		else
 		{
-			musicLoader = new Minim(this);
-			musicPlayer = musicLoader.loadFile(musicFile.getAbsolutePath(),512); //Load the music file into the musicPlayer
+			musicLoader = new Minim(parent);
+			musicPlayer = musicLoader.loadFile(musicFile.getAbsolutePath(),1024); //Load the music file into the musicPlayer
 			fileLoaded = true;
-			println("File loaded");
+			PApplet.println("File loaded");
 			parseMetaData(); //Only want to call these 3 methods once
 			setupSpectrum(); //Having them in setup() was causing the rest of the program not to work
 			playMusicFile();
@@ -71,13 +94,13 @@ public class Loader extends PApplet{
 			 * File is loaded so we have the data we need to draw the spectrum 
 			 * so re-enable looping of the draw() method
 			 */
-			loop(); 
+			parent.loop(); 
 		}
 	}
 	 
 	public void parseMetaData()
 	{
-		println("Metadata reached");
+		PApplet.println("Metadata reached");
 		musicMetaData = musicPlayer.getMetaData(); //Gets the metadata for the current file loaded into musicPlayer
 	}
 	
@@ -93,8 +116,8 @@ public class Loader extends PApplet{
 		Just some debugging code to see what is actually getting parsed from the
 		music file
 		*/
-		println(frequencySpectrum.specSize());
-		println(frequencySpectrum.getBand(1));
+		PApplet.println(frequencySpectrum.specSize());
+		PApplet.println(frequencySpectrum.getBand(1));
 	}
 
 	public void playMusicFile()
@@ -109,15 +132,15 @@ public class Loader extends PApplet{
 		 * displayed on the applet and not the console
 		 * 
 		 */
-		background(0);
-		fill(0, 102, 153, 204);
-		text("Filename: " + musicMetaData.fileName(),metaXPos,metaYPos + (metaSpacing * 1));
-		text("Title: " + musicMetaData.title(),metaXPos,metaYPos + (metaSpacing * 2));
-		text("Track: " + musicMetaData.track(),metaXPos,metaYPos + (metaSpacing * 3));
-		text("Artist: " + musicMetaData.author(),metaXPos,metaYPos + (metaSpacing * 4));
-		text("Composer: " + musicMetaData.composer(),metaXPos,metaYPos + (metaSpacing * 5));
-		text("Genre: " + musicMetaData.genre(),metaXPos,metaYPos + (metaSpacing * 6));
-		text("Time: : " + musicMetaData.length(),metaXPos,metaYPos + (metaSpacing * 7));
+		parent.background(0);
+		parent.fill(0, 102, 153, 204);
+		parent.text("Filename: " + musicMetaData.fileName(),metaXPos,metaYPos + (metaSpacing * 1));
+		parent.text("Title: " + musicMetaData.title(),metaXPos,metaYPos + (metaSpacing * 2));
+		parent.text("Track: " + musicMetaData.track(),metaXPos,metaYPos + (metaSpacing * 3));
+		parent.text("Artist: " + musicMetaData.author(),metaXPos,metaYPos + (metaSpacing * 4));
+		parent.text("Composer: " + musicMetaData.composer(),metaXPos,metaYPos + (metaSpacing * 5));
+		parent.text("Genre: " + musicMetaData.genre(),metaXPos,metaYPos + (metaSpacing * 6));
+		parent.text("Time: : " + musicMetaData.length(),metaXPos,metaYPos + (metaSpacing * 7));
 	}
 	
 	public void displayWaveForm()
@@ -126,11 +149,11 @@ public class Loader extends PApplet{
 		 * Spawn sound demon, needs to be fixed, most likely to do with the buffer length
 		 * set when file is being read in
 		 */
-		stroke(255);
+		parent.stroke(255);
 		for(int buffer = 0; buffer < musicPlayer.bufferSize() - 1; ++buffer)
 		{
-			line(buffer,50 + musicPlayer.left.get(buffer) * 50,buffer + 1,musicPlayer.left.get(buffer + 1) * 50);
-			line(buffer,150 + musicPlayer.right.get(buffer) * 50,buffer + 1,150 + musicPlayer.right.get(buffer + 1	) * 50);
+			parent.line(buffer,50 + musicPlayer.left.get(buffer) * 50,buffer + 1,musicPlayer.left.get(buffer + 1) * 50);
+			parent.line(buffer,150 + musicPlayer.right.get(buffer) * 50,buffer + 1,150 + musicPlayer.right.get(buffer + 1	) * 50);
 		}
 	}
 	
